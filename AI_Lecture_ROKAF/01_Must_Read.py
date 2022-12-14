b16078317cc64d66dbc28c3b0cf6eedb64872e76
Hyuntae (Hunter) Yoo <vmfl2098@gmail.com>
2022-12-14 14:53:32 +0000

    It's gonna be fun to study AI

---
+++ AI_Lecture_ROKAF/01_Must_Read.py
@@ -0,0 +1,19 @@
+"""
+Before We Start the Lecture:
+
+중요한 웹사이트들:
+
+1. 파이썬 초보에서 중수가 되기 위한 9가지 스킬 @https://lazymatlab.tistory.com/92
+2. Python bootcamp by 바울랩 (파이썬 기본서) @ https://j.mp/31ZNV5F
+3. Exception and Error Handling in Python @ http://j.mp/2DjHZH5
+4. [***필독 권장***] 개발자를 위한 정보 검색 팁 @ https://j.mp/3j6tLyB
+5. 프로그래머스 인터넷 강의 @ http://j.mp/2vm3Vxo
+6. 점프투파이썬 @ http://j.mp/2XMCOcb
+"""
+
+
+"""
+2. 전자책이다. 제주 방울랩
+5. 인강 (시간이 많이 지나긴 함), 수료증..?
+6. 점프투파이썬 책 읽으며 강의도 듣기.
+"""
