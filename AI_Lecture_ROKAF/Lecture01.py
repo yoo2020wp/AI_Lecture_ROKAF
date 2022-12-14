9b0fbeb27c5a77329b470644b82b1f59ab272092
Hyuntae (Hunter) Yoo <vmfl2098@gmail.com>
2022-12-14 11:57:57 +0000

    Learning AI?

---
+++ AI_Lecture_ROKAF/Lecture01.py
@@ -0,0 +1,22 @@
+print("hello world")
+"""
+ML, Data Analysis 관련 라이브러리 종류:
+
+NumPy -> Numerical Python: 산술연산(행렬 연산)을 효율적으로 할 수 있도록 하는 라이브러리
+
+인공신경망이 모두 행렬연산이다. 그래서 필연적으로 NumPy를 많이 쓸 듯
+
+-------
+
+Pandas -> Panel-data & Series:
+
+Panel-data : 종횡단 데이터(행과 열이 있는 데이터 (엑셀과 같은 정형 데이터))를 쉽게
+             다룰 수 있도록 해주는 라이브러리
+
+matplotlib -> Matrix Plotting Library:
+
+:행렬 기반으로 그래프를 그려주는 라이브러리
+ 데이터 시각화를 위한 라이브러리
+
+"""
+
