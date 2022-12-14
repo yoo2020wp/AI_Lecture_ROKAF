d242152f23cd3e192e0cace5f0f574ecfff532fa
Hyuntae (Hunter) Yoo <vmfl2098@gmail.com>
2022-12-14 13:45:33 +0000

    AI Lecture 01

--- AI_Lecture_ROKAF/Lecture01.py
+++ AI_Lecture_ROKAF/Lecture01.py
@@ -39,6 +39,31 @@ TensorFlow: 딥러닝을 위한 라이브러리
 """
 R의 장점
 
-1. 시각화 (ggplot)
+1. 시각화 (ggplot) {근데 파이썬도 가능}
 2. 고급 통계 기법
+
+R의 제약(단점)
+예측 모델
+"""
+
+
+"""
+**중요**
+
+1. xlsxwriter tutorial로 배우는 Excel(엑셀) 프로그래밍 @https://j.mp/3hNYnBr & https://j.mp/3mBh2UJ
+2. Leveraging the best of both Python and R @ http://j.mp/2QdfYXd & https://j.mp/2WlzS0J
+3. Rvs Python for Data Analysis: An Objective Comparison @ https://j.mp/34zYL3g
+4. 파이썬에서 R의 ggplot2 사용하기: plotnine 라이브러리 @ https://j.mp/38plv64
+5. Machine Learning Open Source of the Month (매달 갱신) @ https://goo.gl/FbXHf3
+어떤 라이브러리를 써야할 지 모르겠을 때
+
+1. Awesome Python @ https://github.com/vinta/awesome-python
+2. Awesome Data Science with Python @ https://github.com/r0f1/datascience
+
+
+주목받는 파이썬 프로젝트들을 둘러보고 싶을 때
+
+1. Github Trending for Python @ https:// github.com/trending/python
+
+
 """
